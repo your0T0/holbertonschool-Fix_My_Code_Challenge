f7227723da659aca5258f663a0c7c0b7be50ac44
your0T0 <yara241020@gmail.com>
2026-06-24 16:49:44 +0300

    task 3

---
+++ challenge/3-user.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+ User Model
+"""
+import hashlib
+import uuid
+
+
+class User():
+    """
+    User class:
+    - id: public string unique (uuid)
+    - password: private string hash in MD5
+    """
+
+    __password = None
+
+    def __init__(self):
+        """
+        Initialize a new user:
+        - assigned an unique `id`
+        """
+        self.id = str(uuid.uuid4())
+
+    @property
+    def password(self):
+        """
+        Password getter
+        """
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """
+        Password setter:
+        - `None` if `pwd` is `None`
+        - `None` if `pwd` is not a string
+        - Hash `pwd` in MD5 before assign to `__password`
+        """
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
+            self.__password = hashlib.md5(
+                pwd.encode()).hexdigest().lower()
+
+    def is_valid_password(self, pwd):
+        """
+        Valid password:
+        - `False` if `pwd` is `None`
+        - `False` if `pwd` is not a string
+        - `False` if `__password` is `None`
+        - Compare `__password` and the MD5 value of `pwd`
+        """
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.__password is None:
+            return False
+
+        return hashlib.md5(
+            pwd.encode()).hexdigest().lower() == self.__password
